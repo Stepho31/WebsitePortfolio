679d619eb0dbcdea0c4c8a4749b5a3001e39af6c
Stephen Byron Cameron Burdine <stephenbyron31@gmail.com>
2024-10-06 00:59:50 -0500

    Fixes

---
+++ Desktop/Portfolio website/portfolio-website/src/assets/mywork_data.js
@@ -0,0 +1,57 @@
+import project1_img from '../assets/project_1.jpg'
+import project2_img from '../assets/project_2.jpg'
+import project3_img from '../assets/project_3.jpg'
+import project4_img from '../assets/project_4.jpg'
+import project5_img from '../assets/project_5.jpg'
+import project6_img from '../assets/project_6.svg'
+
+const mywork_data = [
+    {
+        w_no:1,
+        w_name:"Airline Reservation App",
+        w_img:project2_img,
+        github_link: 'https://github.com/Stepho31/Online-Flight-Reservation-System',
+        title: 'Online Flight Reservation System',
+        tech_stack: 'HTML, CSS, JavaScript, PHP'
+    
+    },
+    {
+        w_no:2,
+        w_name:"Portfolio Website",
+        w_img:project1_img,
+        github_link: 'https://github.com/Stepho31/WebsitePortfolio',
+        title: 'Portfolio Website',
+        tech_stack: 'Javarscript, React, and React Native'
+    },
+    {
+        w_no:3,
+        w_name:"CareerPathway",
+        w_img:project3_img,
+        github_link: 'https://github.com/Stepho31/CareerPathway',
+        title: 'CareerPathway',
+        tech_stack: 'Python'
+    },
+    {
+        w_no:4,
+         w_name:"Storks App",
+        w_img:project4_img,
+        github_link: 'https://github.com/Stepho31/Storks',
+        title: 'Storks (Mobile Dating Application)',
+        tech_stack: 'SwiftUI, Firebase'
+    },
+    {
+        w_no:5,
+        w_name:"2oDoo",
+        github_link: 'https://github.com/Stepho31/2woDoo.git',
+        title: '2oDoo (to-do list application)',
+        tech_stack: 'SwiftUI',
+        w_img:project5_img
+    },
+    {
+        w_no:6,
+        w_name:"Web design",
+        w_img:project6_img
+    },
+]
+ 
+export default mywork_data;
---
+++ Desktop/Portfolio website/portfolio-website/src/assets/services_data.js
@@ -0,0 +1,35 @@
+const Services_Data = [
+    {
+      s_no: "01",
+      s_name: "Mobile App Development (SwiftUI)",
+      s_desc: "I create responsive, high-performance mobile applications using SwiftUI, tailored for iOS devices. Whether it's a new app from scratch or enhancing an existing one, I ensure seamless performance and smooth user interfaces."
+    },
+    {
+      s_no: "02",
+      s_name: "Web Development",
+      s_desc: "Specializing in modern web technologies like JavaScript, HTML, and CSS, I develop scalable and efficient web applications, ensuring robust front-end and back-end integration."
+    },
+    {
+      s_no: "03",
+      s_name: "API Integration & Backend Development",
+      s_desc: "I offer integration with third-party APIs, including REST and GraphQL, along with backend development using Python and C#, providing seamless connections between your app and external services."
+    },
+    {
+      s_no: "04",
+      s_name: "Database Management & SQL",
+      s_desc: "With expertise in SQL and database architecture, I design and optimize databases to ensure efficient data management and retrieval, critical for handling large-scale data-driven applications."
+    },
+    {
+      s_no: "05",
+      s_name: "Digital Marketing Solutions",
+      s_desc: "Combining data-driven strategies and technical expertise, I develop solutions that enhance digital marketing campaigns, leveraging tools like Google Analytics and SEO practices."
+    },
+    {
+      s_no: "06",
+      s_name: "Content Writing & Technical Documentation",
+      s_desc: "I provide technical writing services, creating comprehensive documentation for software systems, API guides, and more, ensuring clarity for end-users and developers alike."
+    }
+  ];
+  
+ 
+export default Services_Data;
